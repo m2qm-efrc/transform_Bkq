722c05405babb1b67e3a5d9cb0a46811c50dc838
Shuanglong Liu <shlliu@physicsd2348a.local>
2025-01-06 16:23:02 -0500

    -

--- get_ITO_CF_parameters.py
+++ get_ITO_CF_parameters.py
@@ -13,6 +13,12 @@ if __name__ == "__main__":
     # Bkqs: crystal field parameters based on extended Stevens operators
     # Bqks: crystal field parameters based on irreducible spherical tensors
 
+    # Suppoted conventions for ITOs:
+    # 1. "spherical": spherical harmonic tensor operators
+    # 2. "Racah": Racah irreducible tensor operators
+    # 3. "Wybourne": Wybourne irreducible tensor operators
+    # 4. "noname": unnamed irreducible tensor operators
+
     # ----------------------------------------------------------------
     # Usage 1: ESO CFP to ITO CFP
     # ----------------------------------------------------------------
